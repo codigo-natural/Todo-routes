8011b908a3dd192a1d8ab9db61b6c081ce003314
Code-Nature <codenature01001@gmail.com>
2023-10-13 13:05:27 -0500

    se añadio un saludo dependiendo la hora en que se encuentre el equipo

--- src/components/TodoHeader.jsx
+++ src/components/TodoHeader.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
 function TodoHeader() {
+  const [mensaje, setMensaje] = useState('');
+
+  useEffect(() => {
+    const hora = new Date().getHours();
+    obtenerMensajeSaludo(hora);
+  }, []);
+
+  const obtenerMensajeSaludo = (hora) => {
+    let mensaje = '';
+
+    if (hora >= 6 && hora < 12) {
+      mensaje = 'Buenos días';
+    } else if (hora >= 12 && hora < 18) {
+      mensaje = 'Buenas tardes';
+    } else {
+      mensaje = 'Buenas noches';
+    }
+
+    setMensaje(mensaje);
+  }
+
   return (
-    <div>TODO APP</div>
-  )
+    <div>
+      <h1>{mensaje} Crack</h1>
+    </div>
+  );
 }
 
 export default TodoHeader
